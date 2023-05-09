fbbd51051e7334bb1d9f48e95e0772000db0e313
Ethan <ethanbastian11@Gmail.com>
2023-05-09 09:48:08 +0700

    remodified html and css, adding a file exploler in the page

---
+++ projects/School Project/11/UniversitySearcher@API/run.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+import json
+
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return "<h1>Ok<h1>"
+
+@app.route("/api/currency/latest")
+def api_currency_latest():
+    try:
+        with open("data.json") as file:
+            data = json.load(file)
+    except:
+        return jsonify({
+            "status" : False
+
+        })
+    else:
+        response = jsonify(data)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+app.run(debug=True)
